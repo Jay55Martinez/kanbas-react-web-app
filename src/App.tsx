77d9b3212d7baf370e6ebb8f369dd9e1afbe301e
Jay55Martinez <martinez.jay@northeastern.edu>
2024-09-19 22:04:21 -0400

    now starts on the sign in page

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Labs from "./Labs";
 import Kanbas from './Kanbas';
-import { HashRouter, Routes, Route, Link } from 'react-router-dom';
+import { HashRouter, Routes, Route, Link, Navigate } from 'react-router-dom';
 import './App.css';
 
 function App() {
@@ -10,6 +10,7 @@ function App() {
       <div>
         <Link to="/Labs">Labs</Link> | <Link to="/Labs/Lab1">Lab1</Link> | <Link to="/Kanbas">Kanbas</Link>
         <Routes>
+          <Route path="/" element={<Navigate to="/Kanbas" />} />
           <Route path="/Labs/*" element={<Labs />} />
           <Route path="/Kanbas/*" element={<Kanbas />} />
         </Routes>
@@ -17,4 +18,5 @@ function App() {
     </HashRouter>
   );
 }
+
 export default App;
