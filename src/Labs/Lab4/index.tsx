2dc961dac23a562016115ecb1a6d9156b7549a9d
Jay55Martinez <martinez.jay@northeastern.edu>
2024-10-25 14:44:47 -0400

    page 8 of a4

--- src/Labs/Lab4/index.tsx
+++ src/Labs/Lab4/index.tsx
@@ -1,9 +1,34 @@
 import ClickEvent from "./ClickEvent";
+import PassingDataOnEvent from "./PassingDataOnEvent";
+import PassingFunctions from "./PassingFunctions";
+import EventObject from "./EventObject";
+import Counter from "./Counter";
+import BooleanStateVariables from "./BooleanStateVariables";
+import StringStateVariables from "./StringStateVariables";
+import DateStateVariable from "./DateStateVariable"
+import ObjectStateVariable from "./ObjectStateVariable";
+import ArrayStateVariable from "./ArrayStateVariable";
+import ParentStateComponent from "./ParentStateComponent";
+
+function sayHello() {
+    alert("Hello!");
+}
+
 export default function Lab4() {
     return (
         <div>
             <h1>Lab 4</h1>
             <ClickEvent />
+            <PassingDataOnEvent />
+            <PassingFunctions theFunction={sayHello} />
+            <EventObject />
+            <Counter />
+            <BooleanStateVariables />
+            <StringStateVariables />
+            <DateStateVariable />
+            <ObjectStateVariable />
+            <ArrayStateVariable />
+            <ParentStateComponent />
         </div>
     );
 }
