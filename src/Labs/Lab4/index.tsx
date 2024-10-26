ac80b279544d3a1ae7c27d6a3024c41e4271f714
Jay55Martinez <martinez.jay@northeastern.edu>
2024-10-26 13:57:40 -0400

    redux part of the lab completed

--- src/Labs/Lab4/index.tsx
+++ src/Labs/Lab4/index.tsx
@@ -9,6 +9,7 @@ import DateStateVariable from "./DateStateVariable"
 import ObjectStateVariable from "./ObjectStateVariable";
 import ArrayStateVariable from "./ArrayStateVariable";
 import ParentStateComponent from "./ParentStateComponent";
+import ReduxExamples from "./ReduxExamples";
 
 function sayHello() {
     alert("Hello!");
@@ -29,6 +30,7 @@ export default function Lab4() {
             <ObjectStateVariable />
             <ArrayStateVariable />
             <ParentStateComponent />
+            <ReduxExamples />
         </div>
     );
 }
