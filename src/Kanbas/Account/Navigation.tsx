68928f439943da2db34575db26546f2fd30d53d6
Jay55Martinez <martinez.jay@northeastern.edu>
2024-10-31 20:10:59 -0400

    completed up to 3.5

--- src/Kanbas/Account/Navigation.tsx
+++ src/Kanbas/Account/Navigation.tsx
@@ -1,10 +1,27 @@
 import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
+
 export default function AccountNavigation() {
+  const { currentUser } = useSelector((state: any) => state.accountReducer);
+  const links = currentUser ? ["Profile"] : ["Signin", "Signup"];
+
   return (
-    <div id="wd-account-navigation" style={{ width: 120 }}
-      className="wd list-group rounded-0 d-none d-md-block">
-      <Link className="list-group-item border-0 active" to="/Kanbas/Account/Signin"  > Signin  </Link> 
-      <Link className="list-group-item border-0 text-danger" to="/Kanbas/Account/Signup"  > Signup  </Link> 
-      <Link className="list-group-item border-0 text-danger" to="/Kanbas/Account/Profile" > Profile </Link> 
+    <div id="wd-account-navigation" style={{ width: 120 }} className="wd list-group rounded-0 d-none d-md-block">
+      {links.includes("Signin") && (
+        <Link className="list-group-item border-0 active" to="/Kanbas/Account/Signin">
+          Signin
+        </Link>
+      )}
+      {links.includes("Signup") && (
+        <Link className="list-group-item border-0 text-danger" to="/Kanbas/Account/Signup">
+          Signup
+        </Link>
+      )}
+      {links.includes("Profile") && (
+        <Link className="list-group-item border-0 text-danger" to="/Kanbas/Account/Profile">
+          Profile
+        </Link>
+      )}
     </div>
-);}
+  );
+}
--- src/Kanbas/Courses/Assignments/AssignmentTable.tsx
+++ src/Kanbas/Courses/Assignments/AssignmentTable.tsx
@@ -7,14 +7,55 @@ import AssignmentButton from "./AssignmentButtons";
 import { Link, useLocation } from "react-router-dom";
 import { useParams } from "react-router";
 import * as db from "../../Database";
+import { useSelector, useDispatch } from "react-redux";
 
 export default function AssignmentTable() {
   const { cid } = useParams();
   const assignments = db.assignments;
   const { pathname } = useLocation();
+  const { currentUser } = useSelector((state: any) => state.accountReducer);
+  if (currentUser.role === "FACULTY") {
   return (
-    <div>
-      <AssignmentControls />
+      <div>
+        <AssignmentControls />
+        <br />
+        <ul id="wd-modules" className="list-group rounded-0">
+          <li className="wd-module list-group-item p-0 mb-5 fs-5 border-gray">
+            <div className="wd-title p-3 ps-2 bg-secondary">
+              <BsGripVertical className="me-2 fs-3" />
+              <IoMdArrowDropdown />
+              Assignments
+              <AssignmentControlButtons />
+            </div>
+            <ul className="wd-lessons list-group rounded-0">
+              {assignments
+                .filter((assignment: any) => assignment.course === cid)
+                .map((assignment: any) => (
+                <li className="wd-lesson list-group-item d-flex align-items-center p-3">
+                  <Link className="btn w-100 text-start d-flex align-items-center" to={`${pathname + '/' + assignment._id}`}>
+                    <BsGripVertical className="me-2 fs-3" />
+                    <PiNotebookLight className="text-success fs-4" />
+                    <div className="ms-2 flex-grow-1">
+                      <span className="d-block font-size-1">{assignment.title}</span>
+                      <span className="d-block font-size-small">
+                        <span className="text-danger">Multiple Modules</span>
+                        <span className="text-black"> | Not available until {new Date(assignment.available).toLocaleDateString("en-US", { month: "long", day: "numeric" })} |</span>
+                      </span>
+                      <span className="d-block font-size-small">Due {new Date(assignment.due).toLocaleDateString("en-US", { month: "long", day: "numeric" })} | {assignment.points} pts</span>
+                    </div>
+                    <AssignmentButton />
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          </li>
+        </ul>
+      </div>
+    );
+  }
+  else {
+    return (
+      <div>
       <br />
       <ul id="wd-modules" className="list-group rounded-0">
         <li className="wd-module list-group-item p-0 mb-5 fs-5 border-gray">
@@ -49,4 +90,5 @@ export default function AssignmentTable() {
       </ul>
     </div>
   );
+  }
 }
